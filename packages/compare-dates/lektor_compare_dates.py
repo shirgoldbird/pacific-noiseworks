9cbd7e467633d0ed7631d5d134764bbe74b5fd90
Shir Goldberg <shirgoldbird@gmail.com>
2019-12-26 22:01:35 -0800

    update test content and event image sizing

--- packages/compare-dates/lektor_compare_dates.py
+++ packages/compare-dates/lektor_compare_dates.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from lektor.types import DateType, DateTimeType
 from jinja2 import Undefined
 
-def current_date():
-    return datetime.now().date()
+def current_datetime():
+    return datetime.now()
 
 def compare_dates(dt1, dt2):
     # check if dt1 > dt2
@@ -15,20 +15,16 @@ class CompareDatesPlugin(Plugin):
     name = 'Compare Dates'
     description = u'Add your description here.'
 
-    def dt1_greater_dt2(self, dt1, dt2):
-        # check if dt1 > dt2
-        return dt1 >= dt2
-
     def on_process_template_context(self, context, **extra):
         def test_function():
             return 'Value from plugin %s' % self.name
         context['test_function'] = test_function
 
     def on_setup_env(self, **extra):
-        self.env.jinja_env.filters['current_date'] = current_date
+        self.env.jinja_env.filters['current_datetime'] = current_datetime
         self.env.jinja_env.filters['compare_dates'] = compare_dates
 
         self.env.jinja_env.globals.update({
-            'current_date': current_date,
+            'current_datetime': current_datetime,
             'compare_dates': compare_dates
         })
