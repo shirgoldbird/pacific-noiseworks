b66f2129fa7cd82a5c110ba029086d501c3d97de
Shir Goldberg <shirgoldbird@gmail.com>
2020-01-26 17:00:24 -0800

    clean up CompareDates package

--- packages/compare-dates/lektor_compare_dates.py
+++ packages/compare-dates/lektor_compare_dates.py
@@ -2,7 +2,6 @@
 from lektor.pluginsystem import Plugin
 from datetime import datetime
 from lektor.types import DateType, DateTimeType
-from jinja2 import Undefined
 
 def current_datetime():
     return datetime.now()
@@ -12,13 +11,8 @@ def compare_dates(dt1, dt2):
     return dt1 >= dt2
 
 class CompareDatesPlugin(Plugin):
-    name = 'Compare Dates'
-    description = u'Add your description here.'
-
-    def on_process_template_context(self, context, **extra):
-        def test_function():
-            return 'Value from plugin %s' % self.name
-        context['test_function'] = test_function
+    name = u'Compare Dates'
+    description = u'Generate the current datetime with current_datetime, or see if one date is >= another with compare_dates'
 
     def on_setup_env(self, **extra):
         self.env.jinja_env.filters['current_datetime'] = current_datetime
